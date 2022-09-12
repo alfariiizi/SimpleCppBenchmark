a7daea4cc290d4f55534eaaf448de07bf9db4461
alfariiizi <alfariiiziiiii@gmail.com>
2022-09-12 23:10:08 +0700

    Added duration that return double type (either in microsecond or in milisecond)

--- SimpleBenchmark.cpp
+++ SimpleBenchmark.cpp
@@ -14,12 +14,21 @@ SimpleBenchmark::~SimpleBenchmark()
 
 void SimpleBenchmark::Stop()
 {
+    std::cout << this->GetDurationMicroSec() << "us (" << this->GetDurationMiliSec() << " ms)\n";
+}
+
+double SimpleBenchmark::GetDurationMicroSec()
+{
     auto endTimePoint = std::chrono::high_resolution_clock::now();
     auto start = std::chrono::time_point_cast<std::chrono::microseconds>( m_startTimePoint ).time_since_epoch().count();
     auto end = std::chrono::time_point_cast<std::chrono::microseconds>( endTimePoint ).time_since_epoch().count();
 
     auto duration = end - start;    // duration in microsecond
-    auto ms = duration * 0.001;     // duration in milisecond
 
-    std::cout << duration << "us (" << ms << " ms)\n";
+    return duration;
+}
+
+double SimpleBenchmark::GetDurationMiliSec()
+{
+    auto ms = this->GetDurationMicroSec() * 0.001;     // duration in milisecond
 }
--- SimpleBenchmark.hpp
+++ SimpleBenchmark.hpp
@@ -9,6 +9,8 @@ public:
     SimpleBenchmark();
     ~SimpleBenchmark();
     void Stop();
+    double GetDurationMicroSec();
+    double GetDurationMiliSec();
 
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> m_startTimePoint;
